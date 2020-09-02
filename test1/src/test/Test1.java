714bf4d7ff0af62dc0a25c7396a82d8b31366204
BuddyKong <976061698@qq.com>
2020-09-02 14:22:57 +0800

    aaa

--- test1/src/test/Test1.java
+++ test1/src/test/Test1.java
@@ -1,7 +1,16 @@
 package test;
 
+import org.junit.Test;
+
 public class Test1 {
+static int arr[]=new int[5];
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+    int x=4,a=4,b=5,c=6;
+    x+=++a+b+++c++;
+        System.out.println(x);
     }
+    //.................
+//v20000000000
+    //3333222222222222222222222222222222
 }
